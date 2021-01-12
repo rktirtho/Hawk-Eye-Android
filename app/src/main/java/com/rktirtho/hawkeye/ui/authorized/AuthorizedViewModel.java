7cc02040b229df85aed880f0fa43dbae60459620
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-12 23:43:27 +0600

    authorized fragment added

---
+++ app/src/main/java/com/rktirtho/hawkeye/ui/authorized/AuthorizedViewModel.java
@@ -0,0 +1,19 @@
+package com.rktirtho.hawkeye.ui.authorized;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AuthorizedViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AuthorizedViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is About fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
