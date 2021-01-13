189b14fe6910f3b0bf7af298f0f949128d1c4b03
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-13 23:41:51 +0600

    Show monitoring done

--- app/src/main/java/com/rktirtho/hawkeye/MonitorShow.java
+++ app/src/main/java/com/rktirtho/hawkeye/MonitorShow.java
@@ -2,13 +2,64 @@ package com.rktirtho.hawkeye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.rktirtho.hawkeye.adapter.MonitoringViewAdapter;
+import com.rktirtho.hawkeye.client.RetrofitClient;
+import com.rktirtho.hawkeye.model.Employees;
+import com.rktirtho.hawkeye.model.MonitoringView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MonitorShow extends AppCompatActivity {
+    ListView accessList;
+    private ProgressDialog progressDoalog;
+    private List<MonitoringView> monitoringViewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor_show);
+        init();
+        progressDoalog = new ProgressDialog(this);
+        progressDoalog.setMax(100);
+        progressDoalog.setMessage("Fatching Data....");
+        progressDoalog.setTitle("Please Wait");
+        progressDoalog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDoalog.show();
+
+        Call<List<MonitoringView>> call = RetrofitClient.getInstance()
+                .getMonitoringService()
+                .getAccessView(4001);
+
+        call.enqueue(new Callback<List<MonitoringView>>() {
+            @Override
+            public void onResponse(Call<List<MonitoringView>> call, Response<List<MonitoringView>> response) {
+                if (response.isSuccessful()){
+                    monitoringViewList = response.body();
+                    MonitoringViewAdapter adapter = new MonitoringViewAdapter(MonitorShow.this, R.layout.model_access, monitoringViewList);
+                    accessList.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<MonitoringView>> call, Throwable t) {
+
+            }
+        });
+
+
+
+    }
+
+    private void init(){
+        accessList = findViewById(R.id.access_list);
+
     }
 }
--- app/src/main/java/com/rktirtho/hawkeye/service/MonitoringService.java
+++ app/src/main/java/com/rktirtho/hawkeye/service/MonitoringService.java
@@ -1,11 +1,13 @@
 package com.rktirtho.hawkeye.service;
 
 import com.rktirtho.hawkeye.model.Employees;
+import com.rktirtho.hawkeye.model.MonitoringView;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface MonitoringService {
 
@@ -23,6 +25,11 @@ public interface MonitoringService {
 
     @GET("")
     Call<List<Employees>> getAccessByEmployeeId();
+
+    @GET("api/monitoring/person/access/auth/{id}")
+    Call<List<MonitoringView>> getAccessView(@Path("id") int id);
+
+
 
 
 
