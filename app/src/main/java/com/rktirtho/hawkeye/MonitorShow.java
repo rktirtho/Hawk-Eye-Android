60c354c0fa345ad7dda9ced0187b0529b79fc3cd
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-13 23:32:42 +0600

    monitorshow activity added

---
+++ app/src/main/java/com/rktirtho/hawkeye/MonitorShow.java
@@ -0,0 +1,14 @@
+package com.rktirtho.hawkeye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonitorShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor_show);
+    }
+}
