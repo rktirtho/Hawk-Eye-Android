f9892144e36bb547e82107f78d494c68ec9ee866
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-14 15:58:04 +0600

    added item click listener in OrganizationEmployee activity

--- app/src/main/java/com/rktirtho/hawkeye/OrganizationEmployee.java
+++ app/src/main/java/com/rktirtho/hawkeye/OrganizationEmployee.java
@@ -2,7 +2,10 @@ package com.rktirtho.hawkeye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.rktirtho.hawkeye.adapter.EmployeesAdapter;
@@ -37,6 +40,19 @@ public class OrganizationEmployee extends AppCompatActivity {
 
                 EmployeesAdapter adapter = new EmployeesAdapter(OrganizationEmployee.this, R.layout.model_employee, employees);
                 orgEmployee.setAdapter(adapter);
+
+                orgEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent intent = new Intent(OrganizationEmployee.this, MonitorShow.class);
+                        intent.putExtra("personId", employees.get(position).getId());
+                        intent.putExtra("name", employees.get(position).getName());
+                        intent.putExtra("orgName", employees.get(position).getOrgName());
+                        intent.putExtra("image", employees.get(position).getImageId());
+                        intent.putExtra("back", "employees");
+                        startActivity(intent);
+                    }
+                });
             }
 
             @Override
