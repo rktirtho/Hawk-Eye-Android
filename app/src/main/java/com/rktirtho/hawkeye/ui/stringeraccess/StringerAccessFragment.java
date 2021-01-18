d543592f814780a24be2ed5d3373807460d7af15
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-18 22:07:44 +0600

    StringerAccessFragment added

---
+++ app/src/main/java/com/rktirtho/hawkeye/ui/stringeraccess/StringerAccessFragment.java
@@ -0,0 +1,92 @@
+package com.rktirtho.hawkeye.ui.stringeraccess;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.rktirtho.hawkeye.MonitorShow;
+import com.rktirtho.hawkeye.R;
+import com.rktirtho.hawkeye.adapter.EmployeesAdapter;
+import com.rktirtho.hawkeye.client.RetrofitClient;
+import com.rktirtho.hawkeye.model.Employees;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class StringerAccessFragment extends Fragment {
+
+    private StringerAccessViewModel homeViewModel;
+
+    private ProgressDialog progressDoalog;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        homeViewModel =
+                new ViewModelProvider(this).get(StringerAccessViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_yesterday, container, false);
+        final ListView listView = root.findViewById(R.id.list);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                progressDoalog = new ProgressDialog(getContext());
+                progressDoalog.setMax(100);
+                progressDoalog.setMessage("Fatching Data....");
+                progressDoalog.setTitle("Please Wait");
+                progressDoalog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDoalog.show();
+
+                Call<List<Employees>> call = RetrofitClient.getInstance()
+                        .getMonitoringService()
+                        .getYesterday();
+
+                call.enqueue(new Callback<List<Employees>>() {
+                    @Override
+                    public void onResponse(Call<List<Employees>> call, Response<List<Employees>> response) {
+                        if (response.isSuccessful()){
+                            List<Employees> employees = response.body();
+                            EmployeesAdapter adapter = new EmployeesAdapter(getContext(), R.layout.model_employee, employees);
+                            listView.setAdapter(adapter);
+                            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Intent intent = new Intent(getContext(), MonitorShow.class);
+                                    intent.putExtra("personId", employees.get(position).getId());
+                                    intent.putExtra("name", employees.get(position).getName());
+                                    intent.putExtra("orgName", employees.get(position).getOrgName());
+                                    intent.putExtra("image", employees.get(position).getImageId());
+                                    intent.putExtra("back", "yesterday");
+                                    startActivity(intent);
+                                }
+                            });
+                        }
+                        progressDoalog.dismiss();
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Employees>> call, Throwable t) {
+                        progressDoalog.dismiss();
+                    }
+                });
+
+//                textView.setText(s);
+
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/rktirtho/hawkeye/ui/stringeraccess/StringerAccessViewModel.java
@@ -0,0 +1,19 @@
+package com.rktirtho.hawkeye.ui.stringeraccess;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StringerAccessViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public StringerAccessViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Yesterday fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
