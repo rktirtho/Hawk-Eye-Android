e465b744500eea891904e1d551f2bff21e52b2f2
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-21 20:43:02 +0600

    Stranger Monitor view activity added

---
+++ app/src/main/java/com/rktirtho/hawkeye/StrangerMotitorShow.java
@@ -0,0 +1,14 @@
+package com.rktirtho.hawkeye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StrangerMotitorShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stranger_motitor_show);
+    }
+}
