d80670172c899467d3757be3ce42966922054255
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-01-21 15:09:58 +0600

    stranger class defined

--- app/src/main/java/com/rktirtho/hawkeye/model/Stranger.java
+++ app/src/main/java/com/rktirtho/hawkeye/model/Stranger.java
@@ -1,4 +1,52 @@
 package com.rktirtho.hawkeye.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Stranger {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("image")
+    @Expose
+    private String image;
+    @SerializedName("time")
+    @Expose
+    private String time;
+    @SerializedName("visited")
+    @Expose
+    private Integer visited;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
 }
--- app/src/main/java/com/rktirtho/hawkeye/service/StrangerService.java
+++ app/src/main/java/com/rktirtho/hawkeye/service/StrangerService.java
@@ -1,4 +1,14 @@
 package com.rktirtho.hawkeye.service;
 
+import com.rktirtho.hawkeye.model.Stranger;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface StrangerService {
+
+    @GET
+    Call<List<Stranger>> getAll();
 }
